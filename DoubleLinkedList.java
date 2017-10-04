4451946af25649e2a2c43a9e68a59cd7ab931cb9
muhaamed <mold81193@gmail.com>
2017-10-04 02:40:12 -0700

    all test pass except 3 efficiency test

--- DoubleLinkedList.java
+++ DoubleLinkedList.java
@@ -6,6 +6,8 @@ import misc.exceptions.NotYetImplementedException;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+// add previous node to add method
+
 
 /**
  * Note: For more info on the expected behavior of your methods, see
@@ -24,22 +26,28 @@ public class DoubleLinkedList<T> implements IList<T> {
         this.back = null;
         this.size = 0;
     }
+    
+   
 
     @Override
     public void add(T item) {
     		Node newNode = new Node(item);
+    		//add and if back  == null
     		if(front == null) {
+    			// delete this 
     			if(back !=null) {
     				throw new AssertionError("back is occupied");
     			}
     			front = newNode;
     			back = front;
+    			front.prev = null;
     			this.size++;
     			
     		}
     		else {
     			back.setNextNode(newNode);
     			newNode.setNextNode(null);
+    			newNode.prev = back;
     			back = newNode;
     			this.size++;
     			
@@ -52,6 +60,8 @@ public class DoubleLinkedList<T> implements IList<T> {
      *
      * @throws EmptyContainerException if the container is empty and there is no element to remove.
      */
+    
+    // remove from back node instead
 
     @Override
     public T remove() {
@@ -144,49 +154,190 @@ public class DoubleLinkedList<T> implements IList<T> {
 
     @Override
     public void insert(int index, T item) {
-    		if(index < 0 || index >= this.size()+1) {
-    			throw new IndexOutOfBoundsException();
-    		}
-    		Node previousHead = front;
-    		if(index == 0) {
-    			Node newHead = new Node(item);
-    			front = newHead;
-    			front.next = previousHead;
-    			this.size++;
-    		}
-    		else {
-    			int counter = 1;
-    			Node prevoiusNode = front;
-    			Node currentNode = front.next;
-    			while(counter <= index) {
-    				if(counter == index) {
-    					if(index > this.size-1) {
-    						Node newNode = new Node(item);
-    						prevoiusNode.next = newNode;
-    						newNode.next = null;
-    						this.size++;
-    						break;
+		//throw new NotYetImplementedException();
+		if(index < 0 || index >= this.size()+1) {
+			throw new IndexOutOfBoundsException();
+		}
+		Node previousHead = front;
+		if(index == 0) {
+			Node newHead = new Node(item);
+			front = newHead;
+			front.next = previousHead;
+			this.size++;
+		}
+		else {
+			int counter = 1;
+			Node prevoiusNode = front;
+			Node currentNode = front.next;
+			while(counter <= index) {
+				if(counter == index) {
+					if(index > this.size-1) {
+						Node newNode = new Node(item);
+						prevoiusNode.next = newNode;
+						newNode.next = null;
+						this.size++;
+						break;
+				}
+					
+					if(currentNode.next != null) {
+						Node newNode = new Node(item);
+						prevoiusNode.next = newNode;
+						newNode.next = currentNode;
+						this.size++;
 					}
-						
-    					if(currentNode.next != null) {
-    						Node newNode = new Node(item);
-    						prevoiusNode.next = newNode;
-    						newNode.next = currentNode;
-    						this.size++;
-    					}
-    					else {
-    						Node newNode = new Node(item);
-    						prevoiusNode.next = newNode;
-    						newNode.next = null;
-    						this.size++;
-    					}
-    				}
-    				prevoiusNode = currentNode;
-    				currentNode = currentNode.next;
-    				counter++;
-    			}
-    		}
-        //throw new NotYetImplementedException();
+					else {
+						Node newNode = new Node(item);
+						prevoiusNode.next = newNode;
+						newNode.next = null;
+						this.size++;
+					}
+				}
+				prevoiusNode = currentNode;
+				currentNode = currentNode.next;
+				counter++;
+			}
+		}
+//		if(index < 0 || index >= this.size()+1) {
+//		throw new IndexOutOfBoundsException();
+//		}
+//		Node previousHead = front;
+//		if(index == 0) {
+//			System.out.print("hello");
+//			if(front == null) {
+//				
+//				Node newHead = new Node(item);
+//				
+//				front = newHead;
+//				
+//				front.next = null;
+//				front.prev = null;
+//				back = front;
+//				
+//			}
+//			else {
+//				
+//				if(front.next == null) {
+//					Node newHead = new Node(item);
+//					front = newHead;
+//					front.next = null;
+//					front.prev = null;
+//				}
+//				else {
+//					Node newHead = new Node(item);
+//					front = newHead;
+//					front.next = previousHead;
+//					front.prev = null;
+//					previousHead.prev = front;
+//				}
+//			}
+//			
+//			
+//			this.size++;
+//			System.out.println(this.size());
+//		}
+//		else {
+//			System.out.println("not 0");
+//			if(index > this.size()/2) {
+//				System.out.println("index greater than half");
+//				System.out.println(this.size());
+//				Node newNode = new Node(item);
+//				if(index == this.size()) {
+//					if(this.size() == 1) {
+//						newNode.prev = front;
+//						newNode.next = null;
+//						back = newNode;
+//						this.size++;
+//						return;
+//					}
+//					else {
+//						newNode.prev = back;
+//						newNode.next = null;
+//						System.out.println("index greater than half");
+////						if(back == null) {
+////							
+////						}
+//						back.next = newNode;
+//						
+//						back = newNode;
+//						this.size++;
+//						return;
+//					}
+//					
+//				}
+//				else {
+//					Node nextNodeFromBack = back;
+//					Node nodeBefore = back.prev;
+//					int counter = this.size()-1;
+//					while(nextNodeFromBack.prev !=null) {
+//						if(counter == index) {
+//							if(counter == this.size()-1) {
+//								newNode.next = nextNodeFromBack;
+//								newNode.prev = nodeBefore;
+//								back = newNode.next;
+//								back.prev = newNode;
+//								this.size++;
+//							}
+//							else {
+//								newNode.next = nextNodeFromBack;
+//								newNode.prev = nodeBefore;
+//								nodeBefore.next = newNode;
+//								nextNodeFromBack.prev = newNode;
+//								this.size++;
+//							}
+//						
+//						}
+//						nextNodeFromBack = nextNodeFromBack.prev;
+//						nodeBefore = nodeBefore.prev;
+//						counter--;
+//					}
+//					
+//				}
+//			}
+//			else {
+//				System.out.println("not 0 and index not greater than half");
+//	    		if(index == 0) {
+//	    			Node newHead = new Node(item);
+//	    			front = newHead;
+//	    			front.next = previousHead;
+//	    			this.size++;
+//	    		}
+//	    		else {
+//	    			int counter = 1;
+//	    			Node prevoiusNode = front;
+//	    			Node currentNode = front.next;
+//	    			while(counter <= index) {
+//	    				if(counter == index) {
+//	    					if(index > this.size-1) {
+//	    						Node newNode = new Node(item);
+//	    						prevoiusNode.next = newNode;
+//	    						newNode.next = null;
+//	    						this.size++;
+//	    						break;
+//						}
+//							
+//	    					if(currentNode.next != null) {
+//	    						Node newNode = new Node(item);
+//	    						prevoiusNode.next = newNode;
+//	    						newNode.next = currentNode;
+//	    						this.size++;
+//	    					}
+//	    					else {
+//	    						Node newNode = new Node(item);
+//	    						prevoiusNode.next = newNode;
+//	    						newNode.next = null;
+//	    						this.size++;
+//	    					}
+//	    				}
+//	    				prevoiusNode = currentNode;
+//	    				currentNode = currentNode.next;
+//	    				counter++;
+//	    			}
+//	    		}
+//			}
+//		}
+		
+
+		
     }
 
     /**
@@ -198,16 +349,87 @@ public class DoubleLinkedList<T> implements IList<T> {
     
     @Override
     public T delete(int index) {
-    		if(index<0 || index >= this.size()) {
-    			throw new IndexOutOfBoundsException();
+    	if(index <0 || index>=this.size) {
+			throw new IndexOutOfBoundsException();
+		}
+    		Node current = front;
+    		if(index == 0) {
+    			if(front.next!=null) {
+    				front = front.next;
+    				front.prev = null;
+    			}
+    			else {
+    				front = null;
+    			}
+    			this.size--;
+    			return (T) current;
     		}
-    		return null;
-        //throw new NotYetImplementedException();
+    		int counter = 1;
+    		Node previous = front;
+    		current = current.next;
+    		while(counter <= index) {
+    			if(counter == index) {
+    				Node currentBeToBeDeleted = current;
+    				if(current.next != null) {
+    					if(current.prev !=null) {
+        					current.prev.next = current.next;
+    						current.next.prev = current.prev;
+    						this.size--;
+    					}
+    					else {
+    						if(this.size!=1) {
+    							previous.next = previous.next.next;
+    							previous.next.prev = previous;
+    							current = null;
+    							this.size--;
+    							return (T) current;
+    						}
+    						else {
+    							front = current.next;
+        						current = null;
+        						front.prev = null;
+        						
+    						}
+    						this.size--;
+    					}
+    				}
+    				else {
+    					
+    					current = null;
+    					this.size--;
+    				}
+    				
+    				break;
+    			}
+    			current = current.next;
+    			previous = previous.next;
+    			counter++;
+    		}
+    		return (T) current;
     }
+    
+    /**
+     * Returns the index corresponding to the first occurrence of the given item
+     * in the list.
+     *
+     * If the item does not exist in the list, return -1.
+     */
 
     @Override
-    public int indexOf(T item) {
-        throw new NotYetImplementedException();
+    public int indexOf(T item) { 
+    		int index = 0;
+    		Node currentNode = front;
+    		while(currentNode !=null) {
+    			if(currentNode.data == item) {
+    				return index;
+    			}
+    			if(currentNode.data.equals(item)) {
+    				return index;
+    			}
+    			index++;
+    			currentNode = currentNode.next;
+    		}
+    		return -1;
     }
 
     @Override
@@ -215,10 +437,24 @@ public class DoubleLinkedList<T> implements IList<T> {
     		return this.size;
         //throw new NotYetImplementedException();
     }
+    
+    /**
+     * Returns 'true' if this container contains the given element, and 'false' otherwise.
+     */
 
     @Override
     public boolean contains(T other) {
-        throw new NotYetImplementedException();
+		Node currentNode = front;
+		while(currentNode !=null) {
+			if(currentNode.data == other) {
+				return true;
+			}
+			if(currentNode.data.equals(other)) {
+				return true;
+			}
+			currentNode = currentNode.next;
+		}
+		return false;
     }
     
     
@@ -278,7 +514,13 @@ public class DoubleLinkedList<T> implements IList<T> {
          * returns 'false' otherwise.
          */
         public boolean hasNext() {
-            throw new NotYetImplementedException();
+        	//System.out.println(current.data);
+        		if(current == null) {
+        			System.out.println("null ");
+        			return false;
+        		}
+        		return current != null;
+           //throw new NotYetImplementedException();
         }
 
         /**
@@ -289,7 +531,18 @@ public class DoubleLinkedList<T> implements IList<T> {
          *         there are no more elements to look at.
          */
         public T next() {
-            throw new NotYetImplementedException();
+        		
+			
+        		if(current != null) {
+        			T data = current.data;
+        			current = current.next;
+        			return data;
+        		}
+        		else {
+        			throw new NoSuchElementException();
+        		}
+			
+            //throw new NotYetImplementedException();
         }
     }
 }
