e64f6d2f59980522e779be50e1a340bd0aabae5d
Nathan Swanson <swanson4@uw.edu>
2017-10-03 23:15:50 -0700

    Add files via upload
    Implemented constructor, get, put, remove, containsKey and size methods for ArrayDictionary

--- ArrayDictionary.java
+++ ArrayDictionary.java
@@ -11,11 +11,13 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
     // You may not change or rename this field: we will be inspecting
     // it using our private tests.
     private Pair<K, V>[] pairs;
-
-    // You're encouraged to add extra fields (and helper methods) though!
-
+    private int arraySize = 10;
+    private int size;
+       
+     
     public ArrayDictionary() {
-        throw new UnsupportedOperationException();
+       this.pairs = makeArrayOfPairs(arraySize);
+       this.size = 0; 
     }
 
     /**
@@ -40,36 +42,108 @@ public class ArrayDictionary<K, V> implements IDictionary<K, V> {
 
     }
 
+    /**
+     * Returns the value corresponding to the given key.
+     *
+     * @throws NoSuchKeyException if the dictionary does not contain the given key.
+     */
     @Override
-    public V get(K key) {
-        throw new NotYetImplementedException();
+    public V get(K key) {  
+    	if(this.isEmpty()) {
+    		throw new NoSuchKeyException();
+    	} else {
+    		for(int i = 0; i < size; i++) {
+    			K keyValue = pairs[i].key;
+    				if(keyValue == key || keyValue.equals(key) ) {
+    					return pairs[i].value;
+    				}
+    		}
+    		throw new NoSuchKeyException();
+    	}
     }
 
+    /**
+     * Adds the key-value pair to the dictionary. If the key already exists in the dictionary,
+     * replace its value with the given one.
+     */
     @Override
     public void put(K key, V value) {
-        throw new NotYetImplementedException();
+    	if(containsKey(key)) {
+	    	for(int i = 0; i < size; i++) {
+	        	K keyValue = pairs[i].key; 
+				if(keyValue == key || keyValue.equals(key) ) {
+					pairs[i].value = value;
+				}
+	    	}
+    	} else {
+    		if(size == arraySize) {
+    			Pair<K, V>[] newPairArray = makeArrayOfPairs(arraySize * 2);
+    			for(int i = 0; i < size; i++) {
+    				newPairArray[i]= new Pair<K, V>(pairs[i].key, pairs[i].value);
+    			}
+    			pairs = newPairArray;
+    			arraySize = arraySize * 2;
+    		}
+    		pairs[size] = new Pair<K, V>(key, value);
+    		size++;
+    	}
     }
 
+    /**
+     * Remove the key-value pair corresponding to the given key from the dictionary.
+     *
+     * @throws NoSuchKeyException if the dictionary does not contain the given key.
+     */
     @Override
     public V remove(K key) {
-        throw new NotYetImplementedException();
+        if(!containsKey(key)) {
+        	throw new NoSuchKeyException();
+        }
+        V removedValue = null;
+        int index = 0;
+        for(int i = 0; i < size; i++) {
+        	K keyValue = pairs[i].key; 
+			if(keyValue == key || keyValue.equals(key) ) {
+				removedValue = pairs[i].value;
+				pairs[i].key = null;
+				pairs[i].value = null;
+				index = i;
+			}
+		}
+        for(int i = index; i < size; i++) {
+        	pairs[i] = pairs[i + 1];
+        }
+        pairs[size] = null;
+        size--;
+        return removedValue;
     }
-
+ 
+    /**
+     * Returns 'true' if the dictionary contains the given key and 'false' otherwise.
+     */
     @Override
     public boolean containsKey(K key) {
-        throw new NotYetImplementedException();
+    	for(int i = 0; i < size; i++) {
+    		K keyValue = pairs[i].key;
+    		if(keyValue == key || keyValue.equals(key) ) {
+    			return true;
+    		}
+    	}
+        return false;
     }
 
+    /**
+     * Returns the number of key-value pairs stored in this dictionary.
+     */
     @Override
     public int size() {
-        throw new NotYetImplementedException();
+        return this.size;
     }
 
     private static class Pair<K, V> {
         public K key;
         public V value;
 
-        // You may add constructors and methods to this class as necessary.
         public Pair(K key, V value) {
             this.key = key;
             this.value = value;
